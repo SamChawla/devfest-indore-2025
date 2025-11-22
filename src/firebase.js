2c82112e656c176724cd70aecc35308b95ebac09
Ani <turboji007@gmail.com>
2025-11-22 18:33:01 +0530

    added env template

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
+// for env file setup
+// const firebaseConfig = {
+//   apiKey: import.meta.env.FIREBASE_API_KEY,
+//   authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.FIREBASE_APP_ID,
+//   measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
+// };
+
+// hardcoded 
 const firebaseConfig = {
   apiKey: "AIzaSyBCRANhNNegW6JZJeaYzLhipO-FlaUo5rA",
   authDomain: "gdgwtmcloudindore.firebaseapp.com",
