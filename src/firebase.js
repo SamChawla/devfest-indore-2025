767fc0fa342582600d5e9f2663ed2a6f4ae164f0
Ani <turboji007@gmail.com>
2025-11-22 18:42:09 +0530

    refactor: update firebase configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,26 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 // for env file setup
-// const firebaseConfig = {
-//   apiKey: import.meta.env.FIREBASE_API_KEY,
-//   authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: import.meta.env.FIREBASE_PROJECT_ID,
-//   storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.FIREBASE_APP_ID,
-//   measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
-// };
-
-// hardcoded 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCRANhNNegW6JZJeaYzLhipO-FlaUo5rA",
-  authDomain: "gdgwtmcloudindore.firebaseapp.com",
-  projectId: "gdgwtmcloudindore",
-  storageBucket: "gdgwtmcloudindore.firebasestorage.app",
-  messagingSenderId: "648949307797",
-  appId: "1:648949307797:web:8dbfedfc7a63b48bfbbe9c",
-  measurementId: "G-QRVLMN5S1M"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
